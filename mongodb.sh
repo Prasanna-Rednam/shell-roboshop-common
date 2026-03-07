1ed4eaf20542fc3fa7095ea81776255a7fa7c653
Prasanna Rednam <prasannarednam08@gmail.com>
2026-03-07 20:15:58 +0530

    script

--- mongodb.sh
+++ mongodb.sh
@@ -6,21 +6,21 @@ source ./common.sh
 check_root
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongo Repo" 
+validate $? "Copying Mongo Repo" 
 
 dnf install mongodb-org -y &>>$LOGS_FILE
-VALIDATE $? "Installing MongoDB server"
+validate $? "Installing MongoDB server"
 
 systemctl enable mongod &>>$LOGS_FILE
-VALIDATE $? "Enable MongoDB"
+validate $? "Enable MongoDB"
 
 systemctl start mongod
-VALIDATE $? "Start MongoDB"
+validate $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Allowing remote connections"
+validate $? "Allowing remote connections"
 
 systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
+validate $? "Restarted MongoDB"
 
 print_total_time
